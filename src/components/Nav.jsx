cc6b479305d4cdd3a681d2b55ffe44501c1730a8
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-03-08 17:16:19 +0630

    first-commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
+const Nav = ({ libraryStatus, setLibraryStatus }) => {
+  return (
+    <nav>
+      <h1>Amaze</h1>
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+        Library
+        <FontAwesomeIcon icon={faMusic} style={{ marginLeft: "7px" }} />
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/detail.js
@@ -0,0 +1,148 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Kinsfolk",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      artist: "Aarigod",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12997",
+      color: ["#B7D170", "#9D7B5D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Today Feels Like Everyday",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Mama Aiuto",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12128",
+      color: ["#794643", "#F3B878"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Focus",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
+      artist: "Ezzy",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10335",
+      color: ["#794643", "#F3B878"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Flashback",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11225",
+      color: ["#ADA7DF", "#4A407F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Home Court",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      artist: "Blue Wednesday, Shopan",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11233",
+      color: ["#ADA7DF", "#4A407F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Oasis",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      artist: "Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
+      color: ["#6371AD", "#FED36D"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lilo",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      artist: "Middle School, The Field Tapes",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11244",
+      color: ["#24253A", "#E6F1EA"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Feel Free",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      artist: "Makzo",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11771",
+      color: ["#FED36D", "#6371AD"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
